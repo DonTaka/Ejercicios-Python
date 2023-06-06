9d2cce6fb5d3bbed3e74a7fddbef471a87ca0ac3
DonTaka <jo.riquelmees@gmail.com>
2023-06-06 15:56:46 -0400

    Update Bidimensionales.py

--- Bidimensionales.py
+++ Bidimensionales.py
@@ -1,34 +1,51 @@
 import numpy as np
+#Unidimensional
+arreglo = np.arange(1,101)
+print(arreglo)
 
-arreglo = np.array([[1,2,3],[4,5,6],[7,8,9]])
+#Bidimensional
+arreglo = arreglo.reshape((10,10))
+print(arreglo)
+print(arreglo[8,6])
+print(arreglo[3,9])
+print(arreglo[3,-1])
+print(arreglo[9,5])
+print(arreglo[-1,5])
 
-#Arreglo unidimensional
-arr = np.arange(1,101)
-#print(arr)
-#Arreglo bidimensional
-arr = arr.reshape((10,10))
-print(arr)
-#Arreglo Multidimensional
-#arr = arr.reshape((5,5,4))
-#print(arr[0][-1][-1])
-#Shape = Me muestra las filas y columnas del arreglo
-print(arr.shape)
+#Slice
+#Start:Stop:Step
+#Primeras 3 filas y los primeros 5 datos
+#print(arreglo[0:3,0:5])
+
+#Ultimas 3 filas y datos desde el tercero al septimo
+#print(arreglo[-1:-4:-1,2:6])
+
+#Multidimensional
+#arreglo = arreglo.reshape((5,5,4))
+#print(arreglo)
 
 numero = int(input("Ingrese un numero: "))
 for y in range(10):
     for x in range(10):
-        if  arr[y][x]==numero:
-            print(f"El valor de la coordenada : fila {y} columna {x} es {arr[y][x]}")
+        if  arreglo[y][x]==numero:
+            print(f"El valor de la coordenada : fila {y} columna {x} es {arreglo[y][x]}")
+            
+
+arr1 = np.array([[1,2,3],[4,5,6]])
+arr2 = np.array([[10,20,30,40],[50,60,70,80]])
+
+#print(np.concatenate((arr1,arr2),axis=0))
+print(np.concatenate((arr1,arr2),axis=1))
 
 
-#Ejercicio 1 
-#Bidimensional de 3x3 aleatorios de 0 a 100
+#Crear un arreglo de dos dimensiones de tamaño 3 x 3
+#Con elementos aleatorios de números enteros del 0 al 100.
 import random
-
 lista = []
-
 for i in range(9):
     lista.append(random.randint(0,100))
 
 resultado = np.array(lista).reshape((3,3))
 print(resultado)
+
+
