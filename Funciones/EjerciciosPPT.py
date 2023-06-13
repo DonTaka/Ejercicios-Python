81a4423da2638c48d4bc03fef88688e13455f27c
DonTaka <jo.riquelmees@gmail.com>
2023-06-13 09:30:57 -0400

    Subida Solucion ejercicios PPT

--- Funciones/EjerciciosPPT.py
+++ Funciones/EjerciciosPPT.py
@@ -1,37 +1,51 @@
-#Calculo IVA
+# CalcularIVA
+# IVA = 0.19
+def calcularIVA(precio):
+    total = precio + (precio * 0.19)
+    return total
+
+    # Forma 2
+    # precio*=1.19 return precio
 
 
-def calculoIVA():
-    precio = 50000
-    precio *=1.19
-    print(f"Precio con IVA: ${precio}")
-calculoIVA()
-#Calculo Descuento
-def descuento(desc):
-    precio = 50000
-    precio -=precio*(desc/100)
-    print(f"Precio con descuento {precio}")
-    
-des = int(input("Ingrese cuanto descuento desea: "))
-descuento(des)
-#IMC
-def IMC(peso,estatura):
-    IMC = peso/(estatura**2)
-    print(f"Tiene un IMC de {IMC}")
-    if IMC<18.5:
+precio = int(input("Ingrese precio del producto: "))
+print(f"El total con IVA es ${calcularIVA(precio)}")
+
+
+# CalcularDescuento
+def descuento(precio, desc):
+    porc = desc / 100
+    total = precio - (precio * porc)
+    print(f"El precio total del producto con descuento es ${total}")
+
+
+precio = int(input("Ingrese precio: "))
+desc = int(input("Ingrese descuento(1-99)"))
+descuento(precio, desc)
+
+
+# IMC
+def IMC(peso, estatura):
+    valor = peso / (estatura**2)
+    print(f"Su IMC es de {valor}")
+
+    if valor < 18.5:
         print("Esta bajo peso")
-    elif IMC>18.5 and IMC<=24.9:
+    elif valor >= 18.5 and valor <= 24.9:
         print("Adecuado")
-    elif IMC>=25 and IMC<=29.9:
+    elif valor >= 25 and valor <= 29.9:
         print("Sobrepeso")
-    elif IMC>=30 and IMC<=34.9:
+    elif valor >= 30 and valor <= 34.9:
         print("Obesidad grado 1")
-    elif IMC>=35 and IMC<=39.9:
+    elif valor >= 35 and valor <= 39.9:
         print("Obesidad grado 2")
-    elif IMC>=40:
+    elif valor >= 40:
         print("Obesidad grado 3")
-        
-peso = int(input("Ingrese su peso: "))
-estatura = int(input("Ingrese su estatura"))
+    return valor
 
-IMC(peso,estatura)
+
+peso = int(input("Ingrese su peso: "))
+estatura = float(input("Ingrese su estatura: "))
+
+imc = IMC(peso, estatura)
+print("El IMC es ", imc)
